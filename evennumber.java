733aa7e837e5b2682cbd51390344d42df44b2d02
janhavi-janhavi <98547403+janhavi-janhavi@users.noreply.github.com>
2023-10-16 19:41:15 +0530

    Create evennumber.java

---
+++ evennumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class evenno{
+public static void main(string arge[]){
+  int n;
+  Scanner sc=new Scanner(system.in);
+  int n =sc.nextInt();
+  if(n%2==0){
+    system.out.println("number is even :"+n);
+  }
+  else{
+    system.out.println("number is odd :"+n);
+  }
+}
+}
+  
+  
+  
